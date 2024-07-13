8a5f1a0800517dc57064b1dc0f6be281de0c6a36
LucaB03 <luca.blase10@gmail.com>
2024-07-13 13:35:08 +0200

    Finished LinkedList.py

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -16,7 +16,45 @@ class LinkedList:
             new.next = self.head
             self.head = new
 
+    def search(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return current
+            current = current.next
 
+    def delete(self, data):
+        prev = self.head
+        current = prev.next
+        if prev.data == data:
+            self.head = self.head.next
+            return True
+        while current:
+            if current.data == data:
+                prev.next = current.next if current.next else None
+                return True
+            prev = current
+            current = current.next
+        return False
+
+    def length(self):
+        length = 0
+        current = self.head
+        while current:
+            length += 1
+            current = current.next
+        return length
+
+    def reverse(self):
+        reverse = LinkedList()
+        current = self.head
+        while current:
+            reverse.insert(current.data)
+            current = current.next
+        self.head = reverse.head
+
+
+# Traversing and printing
 llist = LinkedList()
 llist.insert("Hello")
 llist.insert("World")
